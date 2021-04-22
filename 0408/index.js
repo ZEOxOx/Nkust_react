05ffdcd231581060ea8f38ffa32cc2de5dbf95c1
ZEOxOx <67623541+ZEOxOx@users.noreply.github.com>
2021-04-22 09:20:25 +0800

    index.js new
    index.js new - 2021_0422

---
+++ 0408/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App3';
+import App from './appChat';
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
+
+
+
+/*
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(element, document.getElementById('root'));
+*/
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
