10a54eacaa7fff063f656fd28bb480a86252b77d
ZEOxOx <tonychen.mdream1002@gmail.com>
2021-06-08 12:01:34 +0800

    UPDATE 2021/06/08

---
+++ 0415_ticking_clock/index.js
@@ -0,0 +1,9 @@
+// allowsimport React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock from './App';
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
