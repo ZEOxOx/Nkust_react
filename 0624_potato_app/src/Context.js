94a4efd8f9f8ffabf24d71acd1eec60494f06e12
ZEOxOx <67623541+ZEOxOx@users.noreply.github.com>
2021-06-28 10:29:11 +0800

    Delete 0624_potato_app directory

--- 0624_potato_app/src/Context.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const AppContext = React.createContext();
-//Provider
-export const Provider = AppContext.Provider;
-export const Consumer = AppContext.Consumer;
